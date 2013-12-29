d94b19272f48c33feb34566d631f632e8da06151
Alfonso Etxeberria <alfonso@Arambi.local>
2013-12-29 17:30:58 +0100

    añadido Behavior Publishable

---
+++ Model/Behavior/PublishableBehavior.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Publishable behavior class
+ * 
+ * 
+ * @package Cofree.Model.Behavior
+ */
+class PublishableBehavior extends ModelBehavior
+{
+  private $_settings = array();
+  
+  function setup( Model $model, $settings = array()) 
+  {
+    $default = array(
+        'field' => 'published',
+    );
+
+    $this->_settings = (!empty($settings)) ? $settings + $default : $default;
+  }
+
+
+/**
+ * 
+ *
+ * @param object $Model 
+ * @param array $query 
+ * @return array El query para la petición de la búsqueda
+ */
+  function beforeFind( Model $Model, $query)
+  {
+    if( class_exists( 'Router'))
+    {
+      $params = Router::getParams();
+
+      if( !isset( $params ['admin']) && (isset( $params ['controller']) && $params ['controller'] != 'crud'))
+      {
+        $query ['conditions'][$Model->alias .'.'. $this->_settings ['field']] = 1;    
+      }
+    }
+    
+    return $query;  
+  }
+  
+}
+
+?>
